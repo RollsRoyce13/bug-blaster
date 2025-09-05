ab2dfa8056af9e28946055c319b997b2f8537925
RollsRoyce13 <106380187+RollsRoyce13@users.noreply.github.com>
2025-09-05 16:30:32 +0300

    Create TicketItem & TicketList, useEffect for TicketForm & setup ticketReducer states

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./styles.css";
 import { useReducer } from "react";
 import TicketForm from "./components/TicketForm";
 import ticketReducer from "./reducers/ticketReducer";
+import TicketList from "./components/TicketList";
 
 function App() {
-  const initialState = { tickets: [] };
+  const initialState = { tickets: [], editingTicket: null };
 
   const [state, dispatch] = useReducer(ticketReducer, initialState);
 
@@ -13,7 +14,21 @@ function App() {
     <div className="App">
       <div className="container">
         <h1>Bug blaster</h1>
-        <TicketForm dispatch={dispatch}></TicketForm>
+        <TicketForm
+          dispatch={dispatch}
+          editingTicket={state.editingTicket}
+        ></TicketForm>
+
+        {state.tickets.length > 0 && (
+          <div className="results">
+            <h2>All Tickets</h2>
+
+            <TicketList
+              tickets={state.tickets}
+              dispatch={dispatch}
+            ></TicketList>
+          </div>
+        )}
       </div>
     </div>
   );
